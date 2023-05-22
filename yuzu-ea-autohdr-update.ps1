b27c6f1f8e3532f20d4d71223014c3eefa5769b8
OopyDoopy <61489085+OopyDoopy@users.noreply.github.com>
2023-05-22 15:40:39 -0400

    add official ea version

---
+++ yuzu-ea-autohdr-update.ps1
@@ -0,0 +1,30 @@
+$Yuzu = "$env:LOCALAPPDATA\yuzu"
+
+function UpdateYuzu {
+	Start-Process -FilePath "$Yuzu\maintenancetool.exe" -Wait
+}
+
+function YuzuToCemu {
+	Rename-Item -Path "$Yuzu\yuzu-windows-msvc-early-access\yuzu.exe" -NewName "cemu.exe"
+	Rename-Item -Path "$Yuzu\yuzu-windows-msvc-early-access" -NewName "cemu"
+}
+
+function CemuToYuzu {
+	Rename-Item -Path "$Yuzu\cemu\cemu.exe" -NewName "yuzu.exe"
+	Rename-Item -Path "$Yuzu\cemu" -NewName "yuzu-windows-msvc-early-access"
+}
+
+if (Test-Path -Path "$Yuzu\cemu") {
+	if (Test-Path -Path "$Yuzu\cemu\cemu.exe") {
+		CemuToYuzu
+		UpdateYuzu
+		YuzuToCemu
+	}
+}
+
+if (Test-Path -Path "$Yuzu\yuzu-windows-msvc-early-access") {
+	if ( Test-Path -Path "$Yuzu\yuzu-windows-msvc-early-access\yuzu.exe") {
+		UpdateYuzu
+		YuzuToCemu
+	}
+}
